b00f4e4f4abe15bbd72a14c54fd5edd1fc1ed5b4
Steven <ssebexen@gmail.com>
2024-06-04 17:02:34 -0400

    Added Message Component.

--- sb-vite.config.ts
+++ sb-vite.config.ts
@@ -1,17 +1,10 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     // Workaround: GitHub Storybook issue 25154
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
     tsconfigPaths(),
   ],
 });
---
+++ stories/Message.tsx
@@ -0,0 +1,71 @@
+// Whether the Message is the top, middle, or bottom of a "block" of messages from the same user.
+export const enum MessagePosition {
+  Top = 'message-position--top',
+  Middle = 'message-position--middle',
+  Bottom = 'message-position--bottom',
+  Island = 'message-position--island'
+}
+
+interface UserProps {
+  id: number
+  avatarUrl: string
+}
+
+interface MessageProps {
+  timestamp: Date
+  sender: UserProps
+  content: string
+  messagePosition: MessagePosition
+  isCurrentUser: boolean
+  width?: number
+}
+
+function getBorderRadius(isCurrentUser: boolean, messagePosition: MessagePosition): string {
+  if (isCurrentUser) {
+    switch (messagePosition) {
+      case MessagePosition.Top:
+        return '16px 16px 16px 0px';
+        break;
+      case MessagePosition.Middle:
+        return '0px 16px 16px 0px';
+        break;
+      case MessagePosition.Bottom:
+        return '0px 16px 16px 16px';
+        break;
+      case MessagePosition.Island:
+        return '16px 16px 16px 16px';
+    }
+  }
+  else {
+    switch (messagePosition) {
+      case MessagePosition.Top:
+        return '16px 16px 0px 16px';
+        break;
+      case MessagePosition.Middle:
+        return '16px 0px 0px 16px';
+        break;
+      case MessagePosition.Bottom:
+        return '16px 0px 16px 16px';
+        break;
+      case MessagePosition.Island:
+        return '16px 16px 16px 16px';
+        break;
+    }
+  }
+}
+
+export function Message(props: MessageProps) {
+  return (
+    <div className={`flex gap-[23px] ${props.isCurrentUser ? 'flex-row' : 'flex-row-reverse'}`} style={{ width: props.width || 400 }}>
+      <div className="w-[48px] h-[48px] flex-none">
+        <img className="object-cover rounded-full" src={props.sender.avatarUrl}></img>
+      </div>
+      <div className="px-[23px] py-[15px] min-w-0" style={{
+        backgroundColor: (props.isCurrentUser ? '#74C2FF' : '#D9D9D9'),
+        borderRadius: getBorderRadius(props.isCurrentUser, props.messagePosition)
+      }}>
+        <div className="text-[16px] break-words">{props.content}</div>
+      </div>
+    </div>
+  )
+}
