52ef5d1480a991f007974358cb331adffea6734f
Steven <ssebexen@gmail.com>
2024-06-04 13:50:56 -0400

    Fixed click event

--- stories/Task.tsx
+++ stories/Task.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 interface TaskProps {
   name?: string
   description?: string
@@ -6,12 +8,15 @@ interface TaskProps {
 }
 
 export function Task(props: TaskProps) {
+
+  const [ isCompleted, setIsCompleted ] = useState<boolean>(Boolean(props.isCompleted));
+
   return (
-    <div className="border border-[#D8D8D8] rounded-[8px]" style={{ width: props.width, backgroundColor: props.isCompleted ? '#E2FFE5' : '#FFFFFF' }}>
+    <div className="border border-[#D8D8D8] rounded-[8px]" style={{ width: props.width || 300, backgroundColor: isCompleted ? '#E2FFE5' : '#FFFFFF' }}
+      onClick={() => setIsCompleted(!isCompleted)}>
       <div className="flex flex-row">
         <div className="flex pl-[18px] pr-[13px] py-4 justify-center items-center">
-          <input type="checkbox" className="appearance-none flex-0 w-[25px] h-[25px] border border-[#D8D8D8] rounded-[8px] bg-white
-            checked:bg-[#359845]" checked={props.isCompleted}></input>
+          <div className="flex-0 w-[25px] h-[25px] border border-[#D8D8D8] rounded-[8px]" style={{ backgroundColor: isCompleted ? '#359845' : '#FFFFFF' }}></div>
         </div>
         <div className="flex flex-col justify-center items-start">
           <p className="text-[18px]">{props.name}</p>
