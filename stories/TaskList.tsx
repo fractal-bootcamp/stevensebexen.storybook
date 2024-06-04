06341bb940a22e6a5bda2b9a4847e9a69ccb152b
Steven <ssebexen@gmail.com>
2024-06-04 15:20:36 -0400

    Added TaskList Component

---
+++ stories/TaskList.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import { Task, TaskProps } from './Task';
+
+interface TaskListProps {
+  tasks: Array<TaskProps>
+}
+
+function sortTaskList(a: TaskProps, b: TaskProps) {
+  if (a.isCompleted && !b.isCompleted) {
+    return -1;
+  }
+  else if (!a.isCompleted && b.isCompleted) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+export function TaskList(props: TaskListProps) {
+
+  const [taskList, setTaskList] = useState<TaskProps[]>(props.tasks.sort(sortTaskList));
+
+  return (
+    <div className="flex flex-col gap-[16px]">
+      {taskList.map((task, index) => <Task name={task.name} description={task.description} isCompleted={task.isCompleted}
+      onClick={() => {
+        const newTaskList = [...taskList];
+        newTaskList[index].isCompleted = !newTaskList[index].isCompleted;
+        newTaskList.sort(sortTaskList);
+        setTaskList(newTaskList);
+      }}
+      />)}
+    </div>
+  );
+}
