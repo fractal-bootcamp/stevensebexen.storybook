e4ebd5bb022399d1dc30012f34bdb9c243f071b7
Steven <ssebexen@gmail.com>
2024-06-04 18:14:21 -0400

    Added MessageThread

--- stories/Message.tsx
+++ stories/Message.tsx
@@ -11,16 +11,16 @@ interface UserProps {
   avatarUrl: string
 }
 
-interface MessageProps {
+export interface MessageProps {
   timestamp: Date
   sender: UserProps
-  content: string
-  messagePosition: MessagePosition
-  isCurrentUser: boolean
+  content?: string
+  messagePosition?: MessagePosition
+  isCurrentUser?: boolean
   width?: number
 }
 
-function getBorderRadius(isCurrentUser: boolean, messagePosition: MessagePosition): string {
+function getBorderRadius(isCurrentUser: boolean, messagePosition: MessagePosition | undefined): string {
   if (isCurrentUser) {
     switch (messagePosition) {
       case MessagePosition.Top:
@@ -52,17 +52,37 @@ function getBorderRadius(isCurrentUser: boolean, messagePosition: MessagePositio
         break;
     }
   }
+  return '16px 16px 16px 16px';
+}
+
+function getMargins(messagePosition: MessagePosition | undefined) {
+  switch(messagePosition) {
+    case MessagePosition.Top:
+      return '2px 0px 0px 0px';
+      break;
+    case MessagePosition.Middle:
+      return '0px 0px 0px 0px';
+      break;
+    case MessagePosition.Bottom:
+      return '0px 0px 2px 0px';
+      break;
+    case MessagePosition.Island:
+      return '2px 0px 2px 0px';
+      break;
+  }
+  return '0px';
 }
 
 export function Message(props: MessageProps) {
   return (
-    <div className={`flex gap-[23px] ${props.isCurrentUser ? 'flex-row' : 'flex-row-reverse'}`} style={{ width: props.width || 400 }}>
+    <div className={`flex gap-[23px] ${props.isCurrentUser ? 'flex-row' : 'flex-row-reverse'}`} style={{ width: props.width || 400, alignSelf: props.isCurrentUser ? 'flex-start' : 'flex-end'}}>
       <div className="w-[48px] h-[48px] flex-none">
         <img className="object-cover rounded-full" src={props.sender.avatarUrl}></img>
       </div>
       <div className="px-[23px] py-[15px] min-w-0" style={{
         backgroundColor: (props.isCurrentUser ? '#74C2FF' : '#D9D9D9'),
-        borderRadius: getBorderRadius(props.isCurrentUser, props.messagePosition)
+        borderRadius: getBorderRadius(Boolean(props.isCurrentUser), props.messagePosition),
+        margin: getMargins(props.messagePosition)
       }}>
         <div className="text-[16px] break-words">{props.content}</div>
       </div>
---
+++ stories/MessageThread.stories.ts
@@ -0,0 +1,72 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { MessageThread } from './MessageThread';
+
+const meta: Meta<typeof MessageThread> = {
+  title: 'Example/MessageThread',
+  component: MessageThread,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MessageThread>;
+
+export const Primary: Story = {
+  args: {
+    currentUserId: 1,
+    messages: [
+      {
+        timestamp: new Date(100),
+        sender: {
+          id: 1,
+          avatarUrl: 'img/avatar1.webp'
+        },
+        content: 'I am a placeholder dragon.',
+      },
+      {
+        timestamp: new Date(98),
+        sender: {
+          id: 1,
+          avatarUrl: 'img/avatar1.webp'
+        },
+        content: 'Hello there!',
+      },
+      {
+        timestamp: new Date(102),
+        sender: {
+          id: 1,
+          avatarUrl: 'img/avatar1.webp'
+        },
+        content: 'I am here to help test this system!',
+      },
+      {
+        timestamp: new Date(105),
+        sender: {
+          id: 2,
+          avatarUrl: 'img/avatar2.webp'
+        },
+        content: 'Party rockers in the',
+      },
+      {
+        timestamp: new Date(108),
+        sender: {
+          id: 2,
+          avatarUrl: 'img/avatar2.webp'
+        },
+        content: 'House tonight.'
+      },
+      {
+        timestamp: new Date(94),
+        sender: {
+          id: 3,
+          avatarUrl: 'img/avatar3.png'
+        },
+        content: 'According to all known laws of aviation, there\'s no way a bee should be able to fly. I don\'t actually have this memorized I really just need a long test message.'
+      }
+    ]
+  }
+}
