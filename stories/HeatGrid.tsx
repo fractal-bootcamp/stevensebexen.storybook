4168c2424398e800e4d25e3377a4587fa3f92a86
Steven <ssebexen@gmail.com>
2024-06-05 16:09:16 -0400

    Refactor Color to class

--- stories/HeatGrid.tsx
+++ stories/HeatGrid.tsx
@@ -1,4 +1,4 @@
-import { Color, colorFromString, computeLinearGradientAtPos, stringFromColor } from '../utils/Color';
+import { Color } from '../utils/Color';
 
 interface HeatGridProps {
   data: Array<number>,
@@ -12,8 +12,8 @@ interface HeatGridProps {
 }
 
 export function HeatGrid(props: HeatGridProps) {
-  const colorMin: Color = colorFromString(props.colorMin);
-  const colorMax: Color = colorFromString(props.colorMax);
+  const colorMin: Color = Color.fromString(props.colorMin);
+  const colorMax: Color = Color.fromString(props.colorMax);
   const hasLabels = props.rowLabels !== undefined;
 
   return (
@@ -22,7 +22,7 @@ export function HeatGrid(props: HeatGridProps) {
         {props.rowLabels?.map(label => <p className="basis-full text-[16px] text-center">{label}</p>)}
       </div>
       <div className={`grid grow gap-[10px] grid-rows-${props.numRows} grid-cols-${props.numCols}`}>
-        {Array(props.numCols * props.numRows).fill(0).map((box, index) => <div style={{ backgroundColor: stringFromColor(computeLinearGradientAtPos(props.data[index], colorMin, colorMax))}}></div>)}
+        {Array(props.numCols * props.numRows).fill(0).map((box, index) => <div style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
       </div>
     </div>
   )
--- utils/Color.ts
+++ utils/Color.ts
@@ -1,29 +1,34 @@
-// TODO: Replace with a proper class
+export class Color {
 
-export interface Color {
-  r: number,
-  g: number,
+  r: number
+  g: number
   b: number
-}
 
-export function colorFromString(str: string): Color {
-  return {
-    r: Number('0x' + str.slice(1, 3)),
-    g: Number('0x' + str.slice(3, 5)),
-    b: Number('0x' + str.slice(5))
-  };
-}
+  constructor (r: number, g: number, b: number) {
+    this.r = Math.round(Math.max(0, Math.min(255, r)));
+    this.g = Math.round(Math.max(0, Math.min(255, g)));
+    this.b = Math.round(Math.max(0, Math.min(255, b)));
+  }
 
-export function stringFromColor(col: Color): string {
-  console.log(col);
-  return '#' + Math.round(col.r).toString(16).padStart(2, '0') + Math.round(col.g).toString(16).padStart(2, '0') + Math.round(col.b).toString(16).padStart(2, '0');
-}
+  static fromString(str: string): Color {
+    return new Color(
+      Number('0x' + str.slice(1, 3)),
+      Number('0x' + str.slice(3, 5)),
+      Number('0x' + str.slice(5))
+    );
+  }
+  
+  static interpolateLinear(pos: number, col1: Color, col2: Color): Color {
+    const ipos: number = 1 - pos;
+    return new Color(
+      col1.r * pos + col2.r * ipos,
+      col1.g * pos + col2.g * ipos,
+      col1.b * pos + col2.b * ipos
+    )
+  }
 
-export function computeLinearGradientAtPos(pos: number, col1: Color, col2: Color): Color {
-  const ipos: number = 1 - pos;
-  return {
-    r: col1.r * pos + col2.r * ipos,
-    g: col1.g * pos + col2.g * ipos,
-    b: col1.b * pos + col2.b * ipos
-  };
+  toString(): string {
+    return '#' + Math.round(this.r).toString(16).padStart(2, '0') + Math.round(this.g).toString(16).padStart(2, '0') + Math.round(this.b).toString(16).padStart(2, '0');
+  }
+
 }
