6d6b283b7f9f43a8905fab8c92ef4e70a5efd9fd
Steven <ssebexen@gmail.com>
2024-06-05 16:15:37 -0400

    Unrelated: removed unnecessary, unimplemented prop that was breaking stuff.

--- stories/HeatGrid.tsx
+++ stories/HeatGrid.tsx
@@ -4,8 +4,6 @@ interface HeatGridProps {
   data: Array<number>,
   colorMin: string,
   colorMax: string,
-  numCols: number,
-  numRows: number,
   rowLabels?: Array<string>
   width?: number,
   height?: number,
@@ -21,8 +19,8 @@ export function HeatGrid(props: HeatGridProps) {
       <div className="flex gap-[2px] flex-row justify-between">
         {props.rowLabels?.map(label => <p className="basis-full text-[16px] text-center">{label}</p>)}
       </div>
-      <div className={`grid grow gap-[10px] grid-rows-${props.numRows} grid-cols-${props.numCols}`}>
-        {Array(props.numCols * props.numRows).fill(0).map((box, index) => <div style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
+      <div className={`grid grow gap-[10px] grid-rows-5 grid-cols-7`}>
+        {Array(35).fill(0).map((box, index) => <div style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
       </div>
     </div>
   )
