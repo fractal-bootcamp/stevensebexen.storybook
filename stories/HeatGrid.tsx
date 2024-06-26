7956f8aa19fa7bcc414f132ad9761accb100930a
ssebexen <167637495+ssebexen@users.noreply.github.com>
2024-06-26 13:29:59 -0400

    Handle undefined case
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- stories/HeatGrid.tsx
+++ stories/HeatGrid.tsx
@@ -19,7 +19,7 @@ export function HeatGrid(props: HeatGridProps) {
   return (
     <div className="flex flex-col gap-[2px]" style={{ width: props.width, height: props.height }}>
       <div className="flex gap-[2px] flex-row justify-between">
-        {Array(props.numCols).fill(0).map((_, i) => <p className="basis-full text-[16px] text-center">{props.rowLabels?.[i]}</p>)}
+        {Array(props.numCols).fill(0).map((_, i) => <p className="basis-full text-[16px] text-center">{props.rowLabels?.[i] || ''}</p>)}
       </div>
       <div className={`grid flex-1 gap-[10px]`} style={{gridTemplateRows: `repeat(${props.numRows}, minmax(0, 1fr))`, gridTemplateColumns: `repeat(${props.numCols}, minmax(0, 1fr))`}}>
         {Array(props.numCols * props.numRows).fill(0).map((box, index) => <div className='basis-full' style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
