3ad6de7a5a4c1ee271490875282135ea076b11f6
Steven <ssebexen@gmail.com>
2024-06-26 12:23:20 -0400

    Removed default stories, made HeatGrid customizable, removed repeating profile pictures on MessageThread, changed border colors on TaskList.

--- stories/HeatGrid.tsx
+++ stories/HeatGrid.tsx
@@ -7,6 +7,8 @@ interface HeatGridProps {
   rowLabels?: Array<string>
   width?: number,
   height?: number,
+  numRows: number,
+  numCols: number
 }
 
 export function HeatGrid(props: HeatGridProps) {
@@ -17,10 +19,10 @@ export function HeatGrid(props: HeatGridProps) {
   return (
     <div className="flex flex-col gap-[2px]" style={{ width: props.width, height: props.height }}>
       <div className="flex gap-[2px] flex-row justify-between">
-        {props.rowLabels?.map(label => <p className="basis-full text-[16px] text-center">{label}</p>)}
+        {Array(props.numCols).fill(0).map((_, i) => <p className="basis-full text-[16px] text-center">{props.rowLabels?.[i]}</p>)}
       </div>
-      <div className={`grid grow gap-[10px] grid-rows-5 grid-cols-7`}>
-        {Array(35).fill(0).map((box, index) => <div style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
+      <div className={`grid flex-1 gap-[10px]`} style={{gridTemplateRows: `repeat(${props.numRows}, minmax(0, 1fr))`, gridTemplateColumns: `repeat(${props.numCols}, minmax(0, 1fr))`}}>
+        {Array(props.numCols * props.numRows).fill(0).map((box, index) => <div className='basis-full' style={{ backgroundColor: Color.interpolateLinear(props.data[index], colorMin, colorMax).toString()}}></div>)}
       </div>
     </div>
   )
